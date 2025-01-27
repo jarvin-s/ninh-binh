997e663c5432a6c148ffba8b6e1d9bb07eb7127c
Jarvinnn <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2025-01-27 14:31:12 +0100

    config: setup project (eslint, prettier)

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='mx-auto mt-20 flex max-w-xl items-center justify-center border-4'>
+      <h1 className='text-4xl font-bold'>Quán Cơm Ninh Bình</h1>
+    </div>
+  )
+}
+
+export default Hero
